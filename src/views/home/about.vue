5c3a75794d88fe85463516247f03ce85721ef337
quuer <5409600@qq.com>
2021-11-10 20:19:36 +0800

    vue-vue-h5-template

--- src/views/home/about.vue
+++ src/views/home/about.vue
@@ -1,23 +1,23 @@
 <!-- home -->
 <template>
-  <div class="about-container">
-    <div class="warpper">
-      <div class="list">
-        <div class="logo"></div>
-        <div class="demo-home__title">VUE H5开发模板</div>
-        <div class="item">
+  <div class='about-container'>
+    <div class='warpper'>
+      <div class='list'>
+        <div class='logo'></div>
+        <div class='demo-home__title'>VUE H5开发模板</div>
+        <div class='item'>
           项目地址:
-          <a href="https://github.com/sunniejs/vue-h5-template">https://github.com/sunniejs/vue-h5-template</a>
+          <a href='https://github.com/sunniejs/vue-h5-template'>https://github.com/sunniejs/vue-h5-template</a>
         </div>
-        <div class="item">项目作者: sunnie</div>
-        <div class="item"></div>
-        <div class="wechat">
-          <img :src="this.wechat" alt="" />
+        <div class='item'>项目作者: sunnie</div>
+        <div class='item'></div>
+        <div class='wechat'>
+          <img :src='this.wechat' alt='' />
         </div>
-        <div class="item">关注公众号：回复“加群”即可加 前端仙女群</div>
-        <div class="item">
+        <div class='item'> 关注公众号：回复“加群”即可加 前端仙女群</div>
+        <div class='item'>
           {{ userName }}
-          <van-button v-if="userName == ''" type="warning" size="small" @click="doDispatch">快点我~</van-button>
+          <van-button v-if="userName == ''" size='small' type='warning' @click='doDispatch'>快点我~</van-button>
         </div>
       </div>
     </div>
@@ -28,6 +28,7 @@
 // 请求接口
 import { getUserInfo } from '@/api/user.js'
 import { mapGetters } from 'vuex'
+
 export default {
   data() {
     return {
@@ -46,8 +47,8 @@ export default {
       // 请求接口数据，仅作为展示，需要配置src->config下环境文件
       const params = { user: 'sunnie' }
       getUserInfo(params)
-        .then(() => { })
-        .catch(() => { })
+        .then(() => {})
+        .catch(() => {})
     },
     // Action 通过 store.dispatch 方法触发
     doDispatch() {
@@ -59,35 +60,42 @@ export default {
   }
 }
 </script>
-<style lang="scss">
+<style lang='scss'>
 .about-container {
   /* 你的命名空间 */
   background: #fff;
   height: 100vh;
   box-sizing: border-box;
+
   .warpper {
     padding: 50px 12px 12px 12px;
+
     .list {
       display: flex;
       flex-direction: column;
       align-items: center;
       color: #666;
       font-size: 14px;
+
       .demo-home__title {
         margin: 0 0 6px;
         font-size: 32px;
+
         .demo-home__title img,
         .demo-home__title span {
           display: inline-block;
           vertical-align: middle;
         }
       }
+
       .item {
         font-size: 14px;
         line-height: 34px;
+
         a {
           text-decoration: underline;
         }
+
         .van-button {
           /* vant-ui 元素*/
           background: #ff5500;
@@ -97,11 +105,13 @@ export default {
       .logo {
         width: 120px;
         height: 120px;
-        background: url($cdn+'/weapp/logo.png') center / contain no-repeat;
+        background: url($cdn + '/weapp/logo.png') center / contain no-repeat;
       }
+
       .wechat {
         width: 200px;
         height: 200px;
+
         img {
           width: 100%;
           height: auto;
